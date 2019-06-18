7a5f4b04bf76d536e8d202c4f44bba11cff69e54
Haim D <haim11293@aol.com>
2019-06-18 00:49:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,10 @@
 # Your Code Here
 def my_own_map(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length
+    new_array.push(yield(source_array[i]))
+    i += 1
+  end
+  new_array
 end
